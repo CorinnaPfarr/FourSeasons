ee0b0274e7ae7836f43d90307631c5f3d6d0d68b
LAPTOP-PD037D8M\Corin <Corinna.Pfarr@gmail.com>
2019-01-14 20:01:00 +0100

    01_initial commit : basic scene with player movement

---
+++ Assets/PlayerMovement.cs
@@ -0,0 +1,40 @@
+﻿using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    public Rigidbody rb;
+
+  
+
+    // Start is called before the first frame update
+    void Start()
+    {
+     //   rb.AddForce(0, 200, 500);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        if (Input.GetKey("a") | Input.GetKey(KeyCode.LeftArrow))
+        {
+            rb.AddForce(-500 * Time.deltaTime, 0, 0);
+        }
+
+        if (Input.GetKey("d") | Input.GetKey(KeyCode.RightArrow))
+        {
+            rb.AddForce(500 * Time.deltaTime, 0, 0);
+        }
+
+        if (Input.GetKey("s") | Input.GetKey(KeyCode.DownArrow))
+        {
+            rb.AddForce(0, 0, 500 * Time.deltaTime);
+        }
+
+        if (Input.GetKey("w") | Input.GetKey(KeyCode.UpArrow))
+        {
+            rb.AddForce(0, 0, -500 * Time.deltaTime);
+        }
+
+    }
+}
